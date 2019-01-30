e43710e54bf70683968352e2ab40105a5f3f1bdf
kirill-pinigin <kir-pinigin@yandex.ru>
2019-01-30 11:42:02 +0700

    ImagePredictionCSVDataSet

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir',       type = str,   default='./koniq10k_224x224/', help='path to dataset')
 parser.add_argument('--result_dir',     type = str,   default='./RESULTS/', help='path to result')
 parser.add_argument('--predictor',      type = str,   default='MobilePredictor', help='type of image generator')
-parser.add_argument('--activation',     type = str,   default='SILU', help='type of activation')
+parser.add_argument('--activation',     type = str,   default='SELU', help='type of activation')
 parser.add_argument('--criterion',      type = str,   default='BCE', help='type of criterion')
 parser.add_argument('--optimizer',      type = str,   default='Adam', help='type of optimizer')
-parser.add_argument('--lr',             type = float, default=1e-3)
-parser.add_argument('--l2',             type = float, default=1e-3)
+parser.add_argument('--lr',             type = float, default=1e-4)
+parser.add_argument('--l2',             type = float, default=0)
 parser.add_argument('--dropout',        type = float, default=0)
-parser.add_argument('--batch_size',     type = int,   default=16)
+parser.add_argument('--batch_size',     type = int,   default=32)
 parser.add_argument('--epochs',         type = int,   default=64)
 parser.add_argument('--resume_train',   type = bool,  default=True, help='type of training')
 parser.add_argument('--pretrained',     type = bool,  default=True, help='type of training')
@@ -41,12 +41,12 @@ predictor_types = { 'ResidualPredictor'        : ResidualPredictor,
                     'SqueezeShuntPredictor'    : SqueezeShuntPredictor
                     }
 
-activation_types = {'ReLU'     : nn.ReLU(),
+activation_types = {'ReLU'      : nn.ReLU(),
                     'LeakyReLU' : nn.LeakyReLU(),
-                    'PReLU'    : nn.PReLU(),
-                    'ELU'      : nn.ELU(),
-                    'SELU'     : nn.SELU(),
-                    'SILU'     : SILU()
+                    'PReLU'     : nn.PReLU(),
+                    'ELU'       : nn.ELU(),
+                    'SELU'      : nn.SELU(),
+                    'SILU'      : SILU()
               }
 
 criterion_types = {
@@ -56,7 +56,7 @@ criterion_types = {
                     }
 
 optimizer_types = {
-                    'Adam'           : optim.Adam,
+                    'Adam'          : optim.Adam,
                     'RMSprop'       : optim.RMSprop,
                     'SGD'           : optim.SGD
                     }
@@ -100,8 +100,8 @@ framework = DeepImagePrediction(predictor = predictor, criterion = criterion, op
 
 if args.transfer:
     framework.predictor.freeze()
-    framework.optimizer = (optimizer_types[args.optimizer] if args.optimizer in optimizer_types else optimizer_types['Adam'])(predictor.parameters(), lr = args.lr / 10.0)
-    framework.approximate(dataloaders=dataloaders, num_epochs= int(args.epochs / 2) if int(args.epochs / 2) < 12 else 12, resume_train=args.resume_train, dropout_factor=args.dropout)
+    framework.optimizer = (optimizer_types[args.optimizer] if args.optimizer in optimizer_types else optimizer_types['Adam'])(predictor.parameters(), lr = args.lr / 2)
+    framework.approximate(dataloaders=dataloaders, num_epochs= int(args.epochs / 2) if int(args.epochs / 2) < 21 else 21, resume_train=args.resume_train, dropout_factor=args.dropout)
     framework.predictor.unfreeze()
 
 framework.approximate(dataloaders = dataloaders, num_epochs=args.epochs, resume_train=args.resume_train, dropout_factor=args.dropout)
